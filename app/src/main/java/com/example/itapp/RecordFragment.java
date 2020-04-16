614074dcda659edf729a0e4216b1cab746650b2f
SadisticPotato <47981381+KRolla16@users.noreply.github.com>
2020-04-16 19:49:59 +0200

    4/7 added recording option - Files are saved to the phone added timer added permissions

--- app/src/main/java/com/example/itapp/RecordFragment.java
+++ app/src/main/java/com/example/itapp/RecordFragment.java
@@ -1,31 +1,56 @@
 package com.example.itapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.SystemClock;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class RecordFragment extends Fragment implements View.OnClickListener {
 
+    private int PERMISSION_CODE = 21;
     private NavController navController;
 
     private ImageButton listBtn;
     private ImageButton recordBtn;
+    private TextView filenameText;
 
 
     private  boolean isRecording = false;
+
+    private MediaRecorder mediaRecorder;
+    private String recordFile;
+
+    private Chronometer timer;
+
+
+    private String recordPermission = Manifest.permission.RECORD_AUDIO;
+
+
     public RecordFragment() {
         // Required empty public constructor
     }
@@ -47,6 +72,8 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
         navController = Navigation.findNavController(view);
         listBtn = view.findViewById(R.id.record_list_btn);
         recordBtn = view.findViewById(R.id.record_btn);
+        timer = view.findViewById(R.id.record_timer);
+        filenameText = view.findViewById(R.id.record_filename);
 
         listBtn.setOnClickListener(this);
         recordBtn.setOnClickListener(this);
@@ -63,14 +90,65 @@ public class RecordFragment extends Fragment implements View.OnClickListener {
             case R.id.record_btn:
                 if (isRecording){
                     //Stop recording
+                    stopRecording();
+
                     recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn_stopped, null));
                     isRecording = false;
                 }else  {
                     //Start Recording
+                    if (checkPermissions()){
+                        startRecording();
                     recordBtn.setImageDrawable(getResources().getDrawable(R.drawable.record_btn_recording, null));
                     isRecording = true;
+                    }
                 }
                 break;
+        }
+    }
+
+    private void startRecording() {
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.start();
+
+        String recordPath = getActivity().getExternalFilesDir("/").getAbsolutePath();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss", Locale.CANADA);
+        Date now = new Date();
+
+        recordFile = "Recording_" + formatter.format(now) + ".3gp";
+
+        filenameText.setText("Recording, File Name: " + recordFile);
+
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setOutputFile(recordPath + "/" + recordFile);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    private void stopRecording() {
+        timer.stop();
+
+        filenameText.setText("Recording Stopped, File Saved: " + recordFile);
+        mediaRecorder.stop();
+        mediaRecorder.release();
+        mediaRecorder = null;
+    }
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(getContext(), recordPermission) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{recordPermission}, PERMISSION_CODE);
+            return false;
         }
     }
 }
