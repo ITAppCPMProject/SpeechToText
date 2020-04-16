13dde29249ef2f36f0e20b5bc11e3725dd4bc8e1
SadisticPotato <47981381+KRolla16@users.noreply.github.com>
2020-04-16 20:35:03 +0200

    5/7
    Added audio adapter
    Created list of audio

--- app/src/main/java/com/example/itapp/AudioListFragment.java
+++ app/src/main/java/com/example/itapp/AudioListFragment.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -25,6 +26,8 @@ public class AudioListFragment extends Fragment {
     private RecyclerView audioList;
 
     private File[] allFiles;
+
+    private AudioListAdapter audioListAdapter;
 
     public AudioListFragment() {
         // Required empty public constructor
@@ -48,9 +51,16 @@ public class AudioListFragment extends Fragment {
         bottomSheetBehavior = BottomSheetBehavior.from(playerSheet);
         audioList = view.findViewById(R.id.audio_list_view);
 
+
         String path = getActivity().getExternalFilesDir("/").getAbsolutePath();
         File directory = new File(path);
         allFiles = directory.listFiles();
+
+        audioListAdapter = new AudioListAdapter(allFiles);
+
+        audioList.setHasFixedSize(true);
+        audioList.setLayoutManager(new LinearLayoutManager(getContext()));
+        audioList.setAdapter(audioListAdapter);
 
         bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
